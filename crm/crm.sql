23fa2181b94fbc29445a2cd8f728b04c0cac8b5d
dloughborough <david.loughborough@kellogg.northwestern.edu>
2022-01-18 17:29:39 +0000

    answered labs

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,55 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS sales;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS industry_membership;
 
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  role TEXT,
+  company_id INTEGER
+);
+
+CREATE TABLE activities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sales_id INTEGER,
+    contact_id INTEGER,
+    contact_type TEXT,
+    date TEXT,
+    notes TEXT
+);
+
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_name TEXT,
+    sales_revenue TEXT
+);
+
+CREATE TABLE sales (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone_number TEXT
+);
+
+CREATE TABLE industries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    industry TEXT
+);
+
+CREATE TABLE industry_membership (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_id INTEGER,
+    industry_id INTEGER
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,44 @@
+-- Drops existing tables, so we start fresh with each
+-- run of this script
+-- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS followers;
 
+-- CREATE TABLES
+
+CREATE TABLE photos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  time TEXT,
+  filename TEXT
+);
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    location TEXT
+);
+
+CREATE TABLE likes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    photo_id INTEGER,
+    user_id TEXT
+);
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    photo_id INTEGER,
+    user_id INTEGER,
+    comment TEXT
+);
+
+CREATE TABLE followers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id_follower TEXT,
+    user_id_followed TEXT
+);
